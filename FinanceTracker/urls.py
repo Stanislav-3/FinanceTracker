e6bd053110ce0ffad0ca8b7a92966be934c4912a
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2022-05-22 14:25:05 +0300

    fix static root

--- FinanceTracker/urls.py
+++ FinanceTracker/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +23,3 @@ urlpatterns = [
     path('overview/', include('overview.urls')),
     # path('static/images/*.jpg', views.return_image)
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
