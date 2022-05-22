63b22442e185898feef8c8b7a8093f13bdd58a92
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2022-05-22 14:20:24 +0300

    added static to url

--- FinanceTracker/urls.py
+++ FinanceTracker/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,6 @@ urlpatterns = [
     path('overview/', include('overview.urls')),
     # path('static/images/*.jpg', views.return_image)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
